696dbf3f7e9d7c91446a87d515e93545332ec6dd
RoaringInferno <jj2t17@gmail.com>
2024-02-18 21:26:25 -0600

    Template for spider.js database

---
+++ spider.js
@@ -0,0 +1,16 @@
+let options;
+const argsSchema = {
+    //
+}
+
+const servers = [
+    {
+        name: "n00dles",
+        orgname: "Noodle Bar",
+        ram: 4,
+        hack_skill: 1,
+        security: 1,
+        money: "$196.875k",
+        port_requirement: 0
+    }
+]
